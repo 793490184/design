aed8ef6de0cca69532cc4682a19207e521225117
793490184 <793490184@qq.com>
2019-04-07 15:56:37 +0800

    开始food 开发

--- src/main/java/com/restaurant/controller/StoremanController.java
+++ src/main/java/com/restaurant/controller/StoremanController.java
@@ -1,9 +1,55 @@
 package com.restaurant.controller;
 
+import com.restaurant.entity.Food;
+import com.restaurant.servie.StoremanService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @Component
 @RequestMapping("/storeman")
 public class StoremanController {
+
+	@Resource
+	StoremanService storemanService;
+
+	@RequestMapping("/insert.do")
+	@ResponseBody
+	public void insert(Food food) {
+		storemanService.insert(food);
+	}
+
+	@RequestMapping("/remove.do")
+	@ResponseBody
+	public void remove(int id) {
+
+	}
+
+	@RequestMapping("/used.do")
+	@ResponseBody
+	public void used(int id) {
+
+	}
+
+	@RequestMapping("/update.do")
+	@ResponseBody
+	public void update(Food food) {
+
+	}
+
+	@RequestMapping("/select.do")
+	@ResponseBody
+	public List<Food> select(int start, int end) {
+		return null;
+	}
+
+	@RequestMapping("/total.do")
+	@ResponseBody
+	public List<Food> total(String startTime, String endTime, int start, int end) {
+		return null;
+	}
+
 }
--- src/main/java/com/restaurant/servie/impl/StoremanServiceImpl.java
+++ src/main/java/com/restaurant/servie/impl/StoremanServiceImpl.java
@@ -1,40 +1,48 @@
 package com.restaurant.servie.impl;
 
+import com.restaurant.dao.FoodMapper;
 import com.restaurant.entity.Food;
 import com.restaurant.servie.StoremanService;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 @Service
 public class StoremanServiceImpl implements StoremanService {
+
+	@Resource
+	private FoodMapper foodMapper;
+
 	@Override
 	public void insert(Food food) {
-
+		foodMapper.insert(food);
 	}
 
 	@Override
 	public void remove(int id) {
-
+		foodMapper.remove(id);
 	}
 
 	@Override
 	public void used(int id) {
-
+		foodMapper.used(id);
 	}
 
 	@Override
 	public void update(Food food) {
-
+		foodMapper.update(food);
 	}
 
 	@Override
 	public List<Food> select(int start, int end) {
-		return null;
+		List<Food> foods = foodMapper.select(start, end);
+		return foods;
 	}
 
 	@Override
 	public List<Food> total(String startTime, String endTime, int start, int end) {
-		return null;
+		List<Food> foods = foodMapper.total(startTime, endTime, start, end);
+		return foods;
 	}
 }
